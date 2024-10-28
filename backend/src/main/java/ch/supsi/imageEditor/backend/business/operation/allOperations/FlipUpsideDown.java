e83581131c1da9e5f576e2718263969d649ae212
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2024-10-28 09:58:25 +0100

    feature: flip upside down operation implemented

--- backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/FlipUpsideDown.java
+++ backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/FlipUpsideDown.java
@@ -1,11 +1,22 @@
 package ch.supsi.imageEditor.backend.business.operation.allOperations;
 
 import ch.supsi.imageEditor.backend.business.images.AbstractImage;
+import ch.supsi.imageEditor.backend.business.images.Pixel;
 
 public class FlipUpsideDown implements Operation {
     @Override
     public AbstractImage doOperation(AbstractImage image) {
-        System.out.println(getClass().getSimpleName());
+        int width = image.getWidth();
+        int height = image.getHeight();
+        Pixel[][] pixels = image.getPixelMatrix();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height / 2; y++) {
+                Pixel temp = pixels[x][y];
+                pixels[x][y] = pixels[x][height - y - 1];
+                pixels[x][height - y - 1] = temp;
+            }
+        }
+        image.setPixel(pixels);
         return image;
     }
 }
