b243fce234a59e9a0230c470c6c4e365cb3b8fbf
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2024-10-28 09:50:19 +0100

    feature: rotate 90 right operation implemented

--- backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/Rotate90DegreesLeft.java
+++ backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/Rotate90DegreesLeft.java
@@ -4,22 +4,18 @@ import ch.supsi.imageEditor.backend.business.images.AbstractImage;
 import ch.supsi.imageEditor.backend.business.images.Pixel;
 
 public class Rotate90DegreesLeft implements Operation {
-    @Override
     public AbstractImage doOperation(AbstractImage image) {
-        System.out.println(this.getClass().getSimpleName());
-
         int newWidth = image.getHeight();
         int newHeight = image.getWidth();
         Pixel[][] rotatedPixels = new Pixel[newWidth][newHeight];
         for (int x = 0; x < newHeight; x++) {
             for (int y = 0; y < newWidth; y++) {
-                rotatedPixels[newWidth - y - 1][x] = image.getPixel(x, y);
+                rotatedPixels[y][newHeight - x - 1] = image.getPixel(x, y);
             }
         }
         image.setPixel(rotatedPixels);
         image.setWidth(newWidth);
         image.setHeight(newHeight);
-        System.out.println("ROTATE90LEFT");
         return image;
     }
 }
--- backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/Rotate90DegreesRight.java
+++ backend/src/main/java/ch/supsi/imageEditor/backend/business/operation/allOperations/Rotate90DegreesRight.java
@@ -1,11 +1,22 @@
 package ch.supsi.imageEditor.backend.business.operation.allOperations;
 
 import ch.supsi.imageEditor.backend.business.images.AbstractImage;
+import ch.supsi.imageEditor.backend.business.images.Pixel;
 
 public class Rotate90DegreesRight implements Operation {
     @Override
     public AbstractImage doOperation(AbstractImage image) {
-        System.out.println(getClass().getSimpleName());
+        int newWidth = image.getHeight();
+        int newHeight = image.getWidth();
+        Pixel[][] rotatedPixels = new Pixel[newWidth][newHeight];
+        for (int x = 0; x < newHeight; x++) {
+            for (int y = 0; y < newWidth; y++) {
+                rotatedPixels[newWidth - y - 1][x] = image.getPixel(x, y);
+            }
+        }
+        image.setPixel(rotatedPixels);
+        image.setWidth(newWidth);
+        image.setHeight(newHeight);
         return image;
     }
 }
