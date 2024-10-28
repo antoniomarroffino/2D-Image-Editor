a4a9427b6df2c07c12e0c6ef4013f099bd9c21ce
MattiaCainarca <mattia.cainarca@gmail.com>
2024-10-28 11:34:16 +0100

    feature: write test image

--- backend/src/test/java/ch/supsi/imageEditor/backend/ReaderTest.java
+++ backend/src/test/java/ch/supsi/imageEditor/backend/ReaderTest.java
@@ -1,8 +1,9 @@
 package ch.supsi.imageEditor.backend;
 
 import ch.supsi.imageEditor.backend.business.images.AbstractImage;
-import ch.supsi.imageEditor.backend.business.images.ImageReaderFactory;
+import ch.supsi.imageEditor.backend.business.images.ImageFactory;
 import ch.supsi.imageEditor.backend.exception.FormatNotSupportedException;
+import ch.supsi.imageEditor.backend.exception.ImageHeaderUncorrectException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,11 +12,11 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class ReaderTest {
-    private ImageReaderFactory factory;
+    private ImageFactory factory;
 
     @BeforeEach
     public void setUp() {
-        this.factory = ImageReaderFactory.getInstance();
+        this.factory = ImageFactory.getInstance();
     }
 
     @Test
@@ -35,7 +36,7 @@ public class ReaderTest {
             AbstractImage image = this.factory.getImage();
             assertEquals(4, image.getWidth());
             assertEquals(4, image.getHeight());
-        }catch(IOException | FormatNotSupportedException ignored){
+        }catch(IOException | FormatNotSupportedException | ImageHeaderUncorrectException ignored){
             ;
         }
     }
@@ -47,7 +48,7 @@ public class ReaderTest {
             AbstractImage image = this.factory.getImage();
             assertEquals(5, image.getWidth());
             assertEquals(5, image.getHeight());
-        }catch(IOException | FormatNotSupportedException ignored){
+        }catch(IOException | FormatNotSupportedException | ImageHeaderUncorrectException ignored){
             ;
         }
     }
@@ -59,7 +60,7 @@ public class ReaderTest {
             AbstractImage image = this.factory.getImage();
             assertEquals(3, image.getWidth());
             assertEquals(2, image.getHeight());
-        }catch(IOException | FormatNotSupportedException ignored){
+        }catch(IOException | FormatNotSupportedException | ImageHeaderUncorrectException ignored){
             ;
         }
     }
