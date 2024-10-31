1394597a75ac4bca435a231d6ca9088f509fa7d1
luca.fanto <luca.fanto@supsi.ch>
2024-10-31 11:34:56 +0100

    feature: check if pipeline is empty when open new image

--- backend/src/main/java/ch/supsi/imageEditor/backend/application/image/ImageController.java
+++ backend/src/main/java/ch/supsi/imageEditor/backend/application/image/ImageController.java
@@ -6,6 +6,8 @@ import ch.supsi.imageEditor.backend.application.observer.NotificationServiceInte
 import ch.supsi.imageEditor.backend.business.images.AbstractImage;
 import ch.supsi.imageEditor.backend.business.images.ImageFactory;
 import ch.supsi.imageEditor.backend.business.images.ImageFactoryInterface;
+import ch.supsi.imageEditor.backend.business.pipeline.PipelineModel;
+import ch.supsi.imageEditor.backend.business.pipeline.PipelineModelInterface;
 import ch.supsi.imageEditor.backend.exception.FormatNotSupportedException;
 import ch.supsi.imageEditor.backend.exception.ImageHeaderUncorrectException;
 
@@ -17,10 +19,12 @@ public class ImageController implements ImageControllerInterface {
     private static ImageController instance = null;
 
     private final ImageFactoryInterface imageReaderFactory;
+    private final PipelineModelInterface pipelineModel;
     private final NotificationServiceInterface notificationService;
 
     private ImageController() {
         this.imageReaderFactory = ImageFactory.getInstance();
+        this.pipelineModel = PipelineModel.getInstance();
         this.notificationService = NotificationService.getInstance();
     }
 
@@ -32,6 +36,10 @@ public class ImageController implements ImageControllerInterface {
     public void loadImage(String filePath) throws FormatNotSupportedException, IOException, ImageHeaderUncorrectException {
         this.imageReaderFactory.readImage(filePath);
         this.notificationService.notify(EventType.OPEN_IMAGE);
+        if (!this.pipelineModel.getPipeline().isEmpty()) {
+            this.pipelineModel.cleanPipeline();
+            this.notificationService.notify(EventType.CLEAR_PIPELINE);
+        }
     }
 
     @Override
