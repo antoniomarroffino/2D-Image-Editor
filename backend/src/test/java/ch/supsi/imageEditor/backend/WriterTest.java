66824c6f81e82caf18b316ccdc3ed825f3159c32
MattiaCainarca <mattia.cainarca@gmail.com>
2024-10-31 10:47:45 +0100

    fix: test image writer

--- backend/src/test/java/ch/supsi/imageEditor/backend/WriterTest.java
+++ backend/src/test/java/ch/supsi/imageEditor/backend/WriterTest.java
@@ -33,19 +33,30 @@ public class WriterTest {
 
     @Test
     public void testPBMWriteImage() {
-        String result = "P1\r\n# Image edited by 2D Image Editor\r\n4 4\r\n0 1 0 1 1 0 1 0 0 1 0 1 1 0 1 0";;
-        assertEquals(result.trim(), p1.toString().trim());
+        String result = "P1" + System.lineSeparator()
+                + "# Image edited by 2D Image Editor" + System.lineSeparator()
+                + "4 4" + System.lineSeparator()
+                + "0 1 0 1 1 0 1 0 0 1 0 1 1 0 1 0 ";
+        assertEquals(result, p1.toString());
     }
 
     @Test
     public void testPGMWriteImage() {
-        String result = "P2\r\n# Image edited by 2D Image Editor\r\n5 5\r\n255\r\n0 50 100 150 200 50 100 150 200 255 100 150 200 255 200 150 200 255 200 150 200 255 200 150 100";
+        String result = "P2" + System.lineSeparator()
+                + "# Image edited by 2D Image Editor" + System.lineSeparator()
+                + "5 5" + System.lineSeparator()
+                + "255" + System.lineSeparator()
+                + "0 50 100 150 200 50 100 150 200 255 100 150 200 255 200 150 200 255 200 150 200 255 200 150 100 ";
         assertEquals(result.trim(), p2.toString().trim());
     }
 
     @Test
     public void testPPMWriteImage() {
-        String result = "P3\r\n# Image edited by 2D Image Editor\r\n3 2\r\n255\r\n255 0 0 0 255 0 0 0 255 255 255 0 0 255 255 255 0 255";
+        String result = "P3" + System.lineSeparator()
+                + "# Image edited by 2D Image Editor" + System.lineSeparator()
+                + "3 2" + System.lineSeparator()
+                + "255" + System.lineSeparator()
+                + "255 0 0 0 255 0 0 0 255 255 255 0 0 255 255 255 0 255 ";
         assertEquals(result.trim(), p3.toString().trim());
     }
 }
