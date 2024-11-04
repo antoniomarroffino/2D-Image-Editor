b0cd13e7ab7511d56b4ae95d1d8e98aa1aad196b
MattiaCainarca <mattia.cainarca@gmail.com>
2024-11-04 11:02:20 +0100

    feature: add dependencies and base class for GUI test

---
+++ frontend/src/test/java/ch/supsi/imageEditor/frontend/AbstractMainGUITest.java
@@ -0,0 +1,39 @@
+package ch.supsi.imageEditor.frontend;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.logging.Logger;
+
+class AbstractMainGUITest extends ApplicationTest {
+    protected static final int SLEEP_INTERVAL = 666;
+
+    protected static final Logger LOGGER = Logger.getAnonymousLogger();
+
+    protected int stepNo;
+
+    @BeforeAll
+    public static void setupSpec() {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
+    }
+
+    protected void step(final String step, final Runnable runnable) {
+        ++stepNo;
+        LOGGER.info("STEP" + stepNo + ":" + step);
+        runnable.run();
+        LOGGER.info("STEP" + stepNo + ":" + "end");
+    }
+
+    public void start(final Stage stage) throws Exception {
+        final MainFX main = new MainFX();
+        stage.toFront();
+        main.start(stage);
+    }
+}
