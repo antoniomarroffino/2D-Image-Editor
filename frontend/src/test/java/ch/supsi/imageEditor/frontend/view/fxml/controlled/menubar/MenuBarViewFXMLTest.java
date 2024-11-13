309573b5fad0813491a08485bde84ca851f95680
MattiaCainarca <mattia.cainarca@gmail.com>
2024-11-13 15:14:39 +0100

    feature: test view frontend

--- frontend/src/test/java/ch/supsi/imageEditor/frontend/view/fxml/controlled/menubar/MenuBarViewFXMLTest.java
+++ frontend/src/test/java/ch/supsi/imageEditor/frontend/view/fxml/controlled/menubar/MenuBarViewFXMLTest.java
@@ -1,6 +1,8 @@
 package ch.supsi.imageEditor.frontend.view.fxml.controlled.menubar;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 
--- frontend/src/test/java/ch/supsi/imageEditor/frontend/view/fxml/uncontrolled/image/ImageViewFXMLTest.java
+++ frontend/src/test/java/ch/supsi/imageEditor/frontend/view/fxml/uncontrolled/image/ImageViewFXMLTest.java
@@ -1,7 +1,6 @@
 package ch.supsi.imageEditor.frontend.view.fxml.uncontrolled.image;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
